a6936e7c2516f11043d428373b64afb66ef47537
LongSei <74856548+LongSei@users.noreply.github.com>
2021-07-18 11:21:50 +0700

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define faster ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+const int MAX = 205;
+
+void solve() {
+	int n, v;
+	cin >> n >> v;
+	cout << v << endl;
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j <= i - 1; j++) {
+			cout << 0 << " ";
+		}
+		cout << v << endl;
+	}
+}
+
+signed main() {
+	faster;
+	int test; cin >> test;
+	while (test--) {
+		solve();
+	}
+}
