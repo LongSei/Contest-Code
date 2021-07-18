bfde30c592b8356ffdd9ca739bcb1e1ae3b4b978
LongSei <74856548+LongSei@users.noreply.github.com>
2021-07-18 11:14:47 +0700

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define ep emplace_back
+#define faster ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int sum = a + b + c;
+    if (sum - (2 * a) == 0 || sum - (2 * b) == 0 || sum - (2 * c) == 0) {
+        cout << "yes" << endl;
+    }
+    else {
+        cout << "no" << endl;
+    }
+}
+
+signed main() {
+	faster;
+	int test = 2;
+	while (test--) {
+		solve();
+	}
+}
