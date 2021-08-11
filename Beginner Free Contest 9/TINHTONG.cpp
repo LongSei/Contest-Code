d06b0112fe3430a0b8db9eeb01487204aec81f29
LongSei <74856548+LongSei@users.noreply.github.com>
2021-08-11 17:32:27 +0700

    Add files via upload

---
+++ Beginner Free Contest 9/TINHTONG.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define faster ios_base::sync_with_stdio(0); cin.tie(0);
+
+signed main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a * b;
+}
