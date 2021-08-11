760de7879f9f406c6672e2126f707c20891b2238
LongSei <74856548+LongSei@users.noreply.github.com>
2021-08-11 17:09:28 +0700

    Add files via upload

---
+++ Beginner Free Contest 29/DIFNPLE.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define faster ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+signed main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a + b > c + d) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+}
